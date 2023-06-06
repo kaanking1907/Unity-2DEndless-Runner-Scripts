a6067e02b4eeb0ec5ef3b5df1801bf2deaa4a0d9
Kaan Kara <kaan.kara@yahoo.com>
2023-06-06 18:17:14 +0300

    Player movement script
    so with this scripts our player can go to right and jump and with the WhatIsGround section our player can only jump while on the ground( you need to make 2 layers called player and ground after assigning the player layer to the player and the ground layer to your platforms, just assign the ground layer to the WhatIsGround variable in the inspector frame.)

---
+++ PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D myRigidbody;
+
+    public bool grounded; 
+    public LayerMask WhatIsGround;
+
+    private Collider2D myCollider;
+
+    private Animator myAnimator;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+        myCollider = GetComponent<Collider2D>();
+
+        myAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        grounded = Physics2D.IsTouchingLayers(myCollider, WhatIsGround);
+
+        myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+        
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) )
+        {
+            if(grounded)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+            }
+        }
+
+        myAnimator.SetFloat ("Speed", myRigidbody.velocity.x);
+        myAnimator.SetBool ("Grounded", grounded);
+    }
+}
