656642f9c0e0bac37edb9db7b5d1bf1fa38667d1
Kaan Kara <kaan.kara@yahoo.com>
2023-06-15 12:25:37 +0300

    Update ObjectPooler.cs

--- ObjectPooler.cs
+++ ObjectPooler.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class ObjectPooler : MonoBehaviour
 {
+
+    public GameObject pooledObject;
+
+    public int pooledAmount;
+
+    List<GameObject> pooledObjects;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pooledObjects = new List<GameObject>();
+
+        for (int i = 0; 1 <pooledAmount; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(pooledObject);
+            obj.SetActive (false);
+            pooledObjects.Add (obj);
+        }
     }
 
     // Update is called once per frame
