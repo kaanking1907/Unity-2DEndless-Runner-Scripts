e78a977b647232953af75c744bd7046077636e3a
Kaan Kara <kaan.kara@yahoo.com>
2023-06-16 19:14:17 +0300

    Update ObjectPooler.cs

--- ObjectPooler.cs
+++ ObjectPooler.cs
@@ -11,22 +11,32 @@ public class ObjectPooler : MonoBehaviour
 
     List<GameObject> pooledObjects;
 
-    // Start is called before the first frame update
     void Start()
     {
         pooledObjects = new List<GameObject>();
 
-        for (int i = 0; 1 <pooledAmount; i++)
+        for (int i = 0; 1 < pooledAmount; i++)
         {
             GameObject obj = (GameObject)Instantiate(pooledObject);
             obj.SetActive (false);
             pooledObjects.Add (obj);
         }
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public GameObject GetPooledObject()
     {
-        
+        for ( int i = 0; i < pooledObjects.Count; i++)
+        {
+            if(pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+
+        GameObject obj = (GameObject)Instantiate(pooledObject);
+        obj.SetActive (false);
+        pooledObjects.Add (obj);
+        return obj;
+
     }
 }
