008c3e5a87da7eba709dcf3f395fde6dd9396dc4
Kaan Kara <kaan.kara@yahoo.com>
2023-07-07 19:42:23 +0300

    Create pickupPoints.cs

---
+++ pickupPoints.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupPoints : MonoBehaviour
+{
+
+    public int scoreToGive;
+
+    private ScoreManager theScoreManager;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
